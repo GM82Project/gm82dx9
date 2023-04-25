4b76a2dd7be2ab2498244dcc47950504efa18704
omicronrex <65692652+omicronrex@users.noreply.github.com>
2023-04-25 18:00:42 -0300

    nicer looking passthroughs

--- source/ps_pass.hlsl
+++ source/ps_pass.hlsl
@@ -1,19 +1,19 @@
-SamplerState rSampler: register(s0);
-
 struct PS_INPUT {
     float4 position: POSITION0;
     float2 texcoord: TEXCOORD0;
-    float4 color: COLOR;
+    float4 color: COLOR0;
 };
 
 struct PS_OUTPUT {
-    float4 color: COLOR;
+    float4 color: COLOR0;
 };
+
+SamplerState rSampler: register(s0);
 
 PS_OUTPUT main(PS_INPUT input) {
     PS_OUTPUT output;
 
-    float4 albedo = tex2D(rSampler,input.texcoord);
+    float4 albedo = tex2D(rSampler, input.texcoord);
 
     output.color = albedo * input.color;
     
--- source/vs_pass.hlsl
+++ source/vs_pass.hlsl
@@ -1,23 +1,23 @@
 struct VS_INPUT {
-   float4 position: POSITION;
+   float4 position: POSITION0;
    float2 texcoord: TEXCOORD0;
-   float4 color: COLOR;
+   float4 color: COLOR0;
 };
 
 struct VS_OUTPUT {
-   float4 position: POSITION;
+   float4 position: POSITION0;
    float2 texcoord: TEXCOORD0;
-   float4 color: COLOR;
+   float4 color: COLOR0;
 };
 
-matrix rMatrixWVP: register(c0);
+matrix rMatrixWVP;
 
-VS_OUTPUT main (VS_INPUT input) {
+VS_OUTPUT main(VS_INPUT input) {
    VS_OUTPUT output;
    
    output.position = mul(rMatrixWVP, input.position);   
    output.texcoord = input.texcoord;   
-   output.color=input.color;
+   output.color = input.color;
 
    return output;
 }
